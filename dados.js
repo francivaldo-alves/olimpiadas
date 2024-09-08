1ba092e9b4f84ad522444a481d16fe4df837b3ed
Francivaldo Alves de Souza <francivaldo.alves@gmail.com>
2024-09-07 22:24:37 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, também conhecida como Fadinha, é uma skatista profissional brasileira que conquistou o mundo com seu talento precoce. Aos poucos anos, ela já colecionava títulos e patrocinadores, tornando-se um ícone da nova geração do skate. Com seu estilo único e carisma, Raissa é um exemplo de que a idade não é limite para alcançar o sucesso no esporte",
+        link: "https://pt.wikipedia.org/wiki/Raissa_Leal",
+        tags: "skate, fadinha, k8",
+
+    },
+
+    // Novo objeto para Beatriz Souza
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro na categoria +78kg. Sua técnica apurada e força física a tornaram uma das principais atletas do judô mundial, inspirando uma nova geração de judocas brasileiras.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza", // Substitua por um link real para a página da Beatriz Souza
+        tags: "judo judoka, judô",
+    },
+
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que brilhou nos Jogos Olímpicos de Paris 2024, conquistando diversas medalhas. Sua versatilidade e carisma a transformaram em uma das maiores ginastas da história do Brasil. Rebeca é um exemplo de superação e determinação, tendo retornado às competições após uma grave lesão.",
+        link: "https://exemplo.com/rebeca-andrade", // Substitua por um link real para a página da Rebeca Andrade
+        tags: "ginasta ouro",
+    }
+]
